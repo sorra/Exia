060a63a44ff6887eec600a9db67113da8446b1e8
Dongqing Hu <dawnstar@163.com>
2014-09-12 23:40:54 +0800

    Simplify JavaSourceFileFilter code

--- src/github/exia/provided/JavaSourceFileFilter.java
+++ src/github/exia/provided/JavaSourceFileFilter.java
@@ -7,10 +7,6 @@ import github.exia.filewalker.FileFilter;
 public class JavaSourceFileFilter implements FileFilter {
   @Override
   public boolean pass(File file) {
-    final String path = file.getPath();
-    if(path.endsWith(".java") && Character.isUpperCase(file.getName().charAt(0))) {
-      return true;
-    }
-    else return false;
+    return file.getPath().endsWith(".java") && Character.isUpperCase(file.getName().charAt(0));
   }
 }
